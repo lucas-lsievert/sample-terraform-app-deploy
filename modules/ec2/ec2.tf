e19430c55d2b340bfc1f179085ed2702961df3f3
Lucas Sievert <lucas.sievert@contractor-mc1.com>
2022-03-11 23:54:50 -0300

    fix

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -15,9 +15,10 @@ data "aws_ami" "amazon-linux-2" {
 }
 
 resource "aws_instance" "this" {
-    ami           = data.aws_ami.amazon-linux-2.id
-    instance_type = var.instance_type
-    subnet_id     = var.subnet_id
+    ami                    = data.aws_ami.amazon-linux-2.id
+    instance_type          = var.instance_type
+    subnet_id              = var.subnet_id
+    vpc_security_group_ids = [aws_security_group.this.id]
 
     tags = {
         Name = "Sample Application"
