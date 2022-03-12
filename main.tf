7035076cb3d367ea7319f48f9baf616c4eaced95
Lucas Sievert <lucas.sievert@contractor-mc1.com>
2022-03-11 23:45:55 -0300

    add sg

--- main.tf
+++ main.tf
@@ -26,4 +26,5 @@ module "ec2" {
 
     subnet_id     = module.vpc.private_subnet_id
     instance_type = var.instance_type
+    vpc_id        = module.vpc.vpc_id
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,3 +7,8 @@ output "public_subnet_id" {
     description = "Public Subnet ID"
     value       = aws_subnet.public_subnet.id
 }
+
+output "vpc_id" {
+    description = "ID of the created VPC"
+    value       = aws_vpc.this.id
+}
