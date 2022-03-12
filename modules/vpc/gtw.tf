e7618ee9fcb193b05bfe21908eef8426088f5d81
Lucas Sievert <lucas.sievert@contractor-mc1.com>
2022-03-11 22:52:31 -0300

    add tags

--- modules/vpc/gtw.tf
+++ modules/vpc/gtw.tf
@@ -1,14 +1,26 @@
 resource "aws_eip" "this" {
     vpc = true
+
+    tags = {
+        Name = "sample_eip"
+    }
 }
 
 resource "aws_internet_gateway" "this" {
     vpc_id = aws_vpc.this.id
+
+    tags = {
+        Name = "sample_Internet_GTW"
+    }
 }
 
 resource "aws_nat_gateway" "this" {
     allocation_id = aws_eip.this.id
     subnet_id     = aws_subnet.private_subnet.id
+
+    tags = {
+      Name = "sample_Nat_GTW"
+    }
 
     depends_on    = [aws_internet_gateway.this]
 }
--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,3 +1,7 @@
 resource "aws_vpc" "this" {
     cidr_block = var.vpc_cidr_block
+
+    tags = {
+      Name = "sample VPC"
+    }
 }
